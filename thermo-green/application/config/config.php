cb3a7128a7d3b3664245446cd955107f21abe724
Bien Alejandro <bien.alejandro@gmail.com>
2018-05-16 20:12:01 +0800

    config commit

--- thermo-green/application/config/config.php
+++ thermo-green/application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:85/thermo-green';
+// $config['base_url'] = 'http://localhost:85/thermo-green';
+$config['base_url'] = 'https://bienskyy.github.io/thermo-green';
 
 /*
 |--------------------------------------------------------------------------
